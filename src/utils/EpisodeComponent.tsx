792451d302d791b838a399d1ce24f39a2386305e
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-03-21 19:28:39 +0500

    modified interface to be compatible with null data type

--- src/utils/EpisodeComponent.tsx
+++ src/utils/EpisodeComponent.tsx
@@ -7,12 +7,18 @@ export default function EpisodeComponent(): JSX.Element {
   const episodeComponents = episodeData.map((episodeInfo: IEpisode) => {
     episodeDataPreparation(episodeData);
     return (
-      <div key={episodeInfo.id} id={episodeInfo.id.toString()}>
+      <div key={episodeInfo.id} id={episodeInfo.id?.toString()}>
         <h2>
           S{twoDigitConverter(episodeInfo.season)}E
           {twoDigitConverter(episodeInfo.number)} - {episodeInfo.name}
         </h2>
-        <img src={episodeInfo.image.medium} alt={episodeInfo.name + " image"} />
+        {episodeInfo.image && (
+          <img
+            src={episodeInfo.image.medium}
+            alt={episodeInfo.name + " image"}
+          />
+        )}
+        {episodeInfo.image === null && <i>image unavailable</i>}
         <p>{episodeInfo.summary}</p>
         <p>Episode Run-Time: {episodeInfo.runtime}</p>
         <p>Episode Rating: {episodeInfo.rating.average}</p>
