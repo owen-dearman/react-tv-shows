8a8deec95f55314bb33b06d0185a98469b628cb2
Owen Dearman <owendearman@gmail.com>
2022-03-21 10:12:33 +0000

    added JSON data to app

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,8 @@
+//import { IEpisode } from "../utils/IEpisode";
+import episodeData from "../utils/episodes.json";
+
 function MainContent(): JSX.Element {
+  console.log(episodeData);
   return <h1>Hello World</h1>;
 }
 
