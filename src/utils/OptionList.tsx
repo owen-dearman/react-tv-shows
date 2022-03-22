43df80818bd152be33a952295d7a9de17ba83613
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-03-22 14:21:23 +0500

    modified drop-down searching

--- src/utils/OptionList.tsx
+++ src/utils/OptionList.tsx
@@ -3,7 +3,15 @@ import { twoDigitConverter } from "./twoDigitConverter";
 import { IEpisode } from "./IEpisode";
 
 export const optionsList = episodeData.map((episode: IEpisode) => (
-  <option key={episode.id} value={episode.name}>
+  <option
+    key={episode.id}
+    value={
+      "S" +
+      twoDigitConverter(episode.season) +
+      "E" +
+      twoDigitConverter(episode.number)
+    }
+  >
     S{twoDigitConverter(episode.season)}E{twoDigitConverter(episode.number)}
   </option>
 ));
--- src/utils/searchFilteredData.ts
+++ src/utils/searchFilteredData.ts
@@ -1,10 +1,16 @@
 import episodeData from "./episodes.json";
 import { IEpisode } from "./IEpisode";
+import { twoDigitConverter } from "./twoDigitConverter";
 
 export const searchFilteredData = (searchInput: string): IEpisode[] => {
   return episodeData.filter(
     (episode) =>
       episode.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-      episode.summary.toLowerCase().includes(searchInput.toLowerCase())
+      episode.summary.toLowerCase().includes(searchInput.toLowerCase()) ||
+      "S" +
+        twoDigitConverter(episode.season) +
+        "E" +
+        twoDigitConverter(episode.number) ===
+        searchInput
   );
 };
