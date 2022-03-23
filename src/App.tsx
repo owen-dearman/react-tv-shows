7c10ca4ceba3dee9137889a7f71e6750a7ea5c7e
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-03-23 17:16:12 +0500

    moved useEffect and Footer to MainContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
 import MainContent from "./components/MainContent";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { useEffect } from "react";
-import { IEpisode } from "./utils/IEpisode";
-import { useState } from "react";
-import showData from "./utils/shows.json";
-import { showsList } from "./utils/ShowsList";
+
 
 function App(): JSX.Element {
-  const [episodeData, setEpisodeData] = useState<IEpisode[]>([]);
-  const [data, setData] = useState<number>(showData[0].id);
-
-  useEffect(() => {
-    const url = `https://api.tvmaze.com/shows/${data}/episodes`;
-    const fetchData = async () => {
-      const response = await fetch(url);
-      const jsonBody: IEpisode[] = await response.json();
-      setEpisodeData(jsonBody);
-      //console.log(jsonBody);
-    };
-    fetchData();
-  }, [data]);
-
+ 
   return (
     <>
       <Header />
-      <select onChange={(e) => setData(parseInt(e.target.value))}>
-        {showsList()}
-      </select>
-      {MainContent(episodeData)}
-      <Footer episodeData={episodeData} />
+      <MainContent/>
     </>
   );
 }
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,16 +3,40 @@ import { searchFilteredData } from "../utils/searchFilteredData";
 import EpisodeComponent from "../utils/EpisodeComponent";
 import { optionsList } from "../utils/OptionList";
 import { IEpisode } from "../utils/IEpisode";
+import { useEffect } from "react";
+import showData from "../utils/shows.json";
+import { twoDigitConverter } from "../utils/twoDigitConverter";
+import { showsList } from "../utils/ShowsList";
 
-function MainContent(episodeData: IEpisode[]): JSX.Element {
+function MainContent(): JSX.Element {
+  const [episodeData, setEpisodeData] = useState<IEpisode[]>([]);
+  const [data, setData] = useState<number>(showData[0].id);
+
+  useEffect(() => {
+    const url = `https://api.tvmaze.com/shows/${data}/episodes`;
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const jsonBody: IEpisode[] = await response.json();
+      setEpisodeData(jsonBody);
+      //console.log(jsonBody);
+    };
+    fetchData();
+  }, [data]);
+
   const [searchInput, setSearchInput] = useState<string>("");
 
   const filteredData = searchFilteredData(episodeData, searchInput);
+
+  
 
   return (
     <>
       <div style={{ textAlign: "center" }}>
         <hr />
+        <select style={{ fontSize: 20 }} onChange={(e) => setData(parseInt(e.target.value))}>
+        {showsList()}
+      </select>
+      <br/>
         <input
           style={{ fontSize: 20 }}
           placeholder="Search Here"
@@ -33,6 +57,20 @@ function MainContent(episodeData: IEpisode[]): JSX.Element {
         Showing {filteredData.length} of {episodeData.length}
       </p>
       <div className="block">{EpisodeComponent(filteredData, episodeData)}</div>
+      <footer className="subtitle">
+      <hr />
+      The data displayed on this webpage is the property of TVMaze. Please click
+      below to navigate to the official data source for each episode.
+      <br />
+      {episodeData.map((episode) => (
+        <li key={episode.id}>
+          <a href={episode.url}>
+            S{twoDigitConverter(episode.season)}E
+            {twoDigitConverter(episode.number)}
+          </a>
+        </li>
+      ))}
+    </footer>
     </>
   );
 }
